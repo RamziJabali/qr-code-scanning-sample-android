7b9d5759d6a7b296d2a0d8884953173db019e3f1
Ramzi Jabali <ramzi@eljabali.org>
2022-03-25 11:24:03 -0700

    Initial Commit

---
+++ app/src/main/java/qrcode/scanning/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package qrcode.scanning.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
