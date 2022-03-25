13e0664dba540da26e4baad131222cabd1fe9533
Ramzi Jabali <ramzi@eljabali.org>
2022-03-25 13:49:50 -0700

    Add compose gradle dependencies and sorted out build issues

---
+++ app/src/main/java/qrcode/scanning/android/HomeView.kt
@@ -0,0 +1,25 @@
+package qrcode.scanning.android
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeView() {
+    Button(
+        onClick = {},
+        modifier = Modifier.padding(2.dp)
+    ) {
+        Text(
+            text = "Camera",
+            color = Color.Green,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
